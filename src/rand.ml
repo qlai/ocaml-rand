7d515b8e47a913bd165e06b1263bb2fef699bb0c
Qiuying Lai <giulia.lai@gmail.com>
2015-07-06 11:19:37 +0100

    changes

--- src/rand.ml
+++ src/rand.ml
@@ -2,33 +2,29 @@
 
 args
 -out file
--rand file(s)
+-rand file(s)  (Use specified file or files or EGD socket (see RAND_egd) for seeding the random number generator. Multiple files can be specified separated by a OS-dependent character. The separator is ; for MS-Windows, , for OpenVMS, and : for all others.)
 -base64
 -hex
 
 dependencies
 -nocrypto
 -ocaml-hex
-
 *)
 
-
 (*implementation*)
-type base = base64 
-type outfile = out
-type hexe = hex
-
 let rand outfile randfile base hex = 
+
+let generatedRand = Nocrypto.rng.generate (*generate pseudo rand*)
 
 let base_str = 
 	match base with
-		| base64 -> Nocrypto.base64.encode (*some code that encodes randbytes with base 64*)
-		| _ -> Nocrypto.rng.generate (*some arguments*)
+		| "base64" -> Nocrypto.base64.encode generatedRand(*some code that encodes randbytes with base 64*)
+		| "NoBase64" -> generatedRand
 
 let hexed = 
-	match hexe with 
-		|hex -> Hex.of_string hex
-		|_ -> print_endline "Not hexed"
+	match hex with 
+		|"hex" -> Hex.of_string base_str
+		|"NoHexEncoding" -> print_endline "Not hexed"
 
 let save file string =
 let channel = open_out file in
@@ -36,11 +32,11 @@ output_string channel string;
 close_out channel;;
 
 let outputfile =
-	match out with
-		| out -> save outfile randbytes
-		| _ -> print_endline outfile
+	match outfile with
+		| Some x -> if hex = "hex" then save x hexed else save x base_str
+		| "NoOutput" -> if hex = "hex" then print_endline hexed else print_endline base_str 
 
-print_endline hexed;;
+print_endline "execution complete"
 
 (* commandline interface*)
 
@@ -48,27 +44,25 @@ open Cmdliner;;
 
 let outfile =
 	let doc = "Write to file instead of standard output." in
-	Arg.(value & opt None & info ["out"; "outfile"] ~doc)
+	Arg.(value & opt string "NoOutput" & info ["out"; "outfile"] ~doc)
 
 let randfile = 
-	let doc = "to be added"
-	Arg.(value & info ["randfile"] ~doc)
+	let doc = "to be added" in
+	Arg.(value & opt string "file for rand seed" & info ["randfile"] ~doc)
 
 let base = 
-	let doc = "perform base64 encoding on output"
-	Arg.(value & opt & info ["b"; "base64"] ~doc)
+	let doc = "perform base64 encoding on output" in
+	Arg.(value & pos 0 string "NoBase64" & info ["b"; "base64"] ~doc)
 
 let hex = 
-	let doc = "show the output as hex string"
-	Arg.(value & opt & info ["hex"] ~doc)
+	let doc = "show the output as hex string" in
+	Arg.(value & pos 1 string "NoHexEncoding" & info ["hex"] ~doc)
 
 let cmd =
 	let doc = "rand" in
 	let man = [
 	`S "BUGS";
-	`P "Email to <giulia.lai@gmail.com>." ] 
-	
-	in
+	`P "Email to <giulia.lai@gmail.com>." ] in
 Term.(ret (pure rand $ outfile $ randfile $ base $ hex)),
 Term.info "rand" ~version:"0.0" ~doc ~man
 
