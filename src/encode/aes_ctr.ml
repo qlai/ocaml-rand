3b4e0f033414429bb3384866b36037025119a0b4
qlai <giulia.lai@gmail.com>
2015-07-21 20:32:31 +0100

    ctr compiles

--- src/encode/aes_ctr.ml
+++ src/encode/aes_ctr.ml
@@ -11,27 +11,25 @@ let aesctr encode yourkey keyfile counter offset infile outfile =
   let getkey = match keyfile with
   | "NA" -> checkkey yourkey (*TODO: here we need to decide whether key is entered or read from file*)
   | _ -> readfile keyfile in
-  let key = (padding getkey) |> Cstruct.of_string in
+  let key = (padding getkey) |> Cstruct.of_string |> AES.CTR.of_secret in
   let ctr = Rng.generate counter in
   let coding = 
   match encode, offset with 
-  | "E", "NA" -> Some(AES.CTR.encrypt ~key:key ~ctr:ctr (Cstruct.of_string(padding(readfile infile))))
-  | "D", "NA" ->  AES.CTR.decrypt ~key:key ~ctr:ctr (Cstruct.of_string(padding(readfile infile)))
-  | "E", _  -> Some(AES.CTR.encrypt ~key:key ~ctr:ctr ~off:offset (Cstruct.of_string(padding(readfile infile))))
-  | "D", _ -> AES.CTR.decrypt ~key:key ~nonce:nonce ~off:offset (Cstruct.of_string(padding(readfile infile)))
+  | "E", 0 -> AES.CTR.encrypt ~key:key ~ctr:ctr (Cstruct.of_string(padding(readfile infile)))
+  | "D", 0 ->  AES.CTR.decrypt ~key:key ~ctr:ctr (Cstruct.of_string(padding(readfile infile)))
+  | "E", _ -> AES.CTR.encrypt ~key:key ~ctr:ctr ~off:offset (Cstruct.of_string(padding(readfile infile)))
+  | "D", _ -> AES.CTR.decrypt ~key:key ~ctr:ctr ~off:offset (Cstruct.of_string(padding(readfile infile)))
   | _ -> failwith "please enter E or D" in
-  match coding with
-  | None -> failwith "failed"
-  | Some(coding) -> savefile outfile (Cstruct.to_string coding)
+savefile outfile (Cstruct.to_string coding)
 
   (*commandline interface start here*)
 let counter =
   let doc = "counter length" in
   Arg.(value & opt int 16 & info ["c"; "ctr"] ~doc)
 
-let off =
+let offset =
   let doc = "offset" in
-  Arg.(value & opt string "NA" & info ["off"; "offset"] ~doc)
+  Arg.(value & opt int 0 & info ["off"; "offset"] ~doc)
 
 let cmd =
   let doc = "aes_ctr block cipher" in
