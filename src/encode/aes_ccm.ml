3bd142f9cbefac4ed75f6817eb72ff604c371cb8
qlai <giulia.lai@gmail.com>
2015-07-22 03:53:27 +0100

    decrypt of ccm is broken, needs fix

--- src/encode/aes_ccm.ml
+++ src/encode/aes_ccm.ml
@@ -15,16 +15,16 @@ let aesccm encode yourkey keyfile adata infile outfile =
   | _ -> readfile keyfile in
   let ckey = (padding getkey) |> Cstruct.of_string in
   let key = AES.CCM.of_secret ~maclen:16 ckey in
-  let nonce = Rng.generate 16 in
+  let nonce = Rng.generate 10 in
   let coding = 
   match encode, adata with 
   | "E", "NA" -> Some(AES.CCM.encrypt ~key:key ~nonce:nonce (Cstruct.of_string(padding(readfile infile))))
-  | "D", "NA" ->  AES.CCM.decrypt ~key:key ~nonce:nonce (Cstruct.of_string(padding(readfile infile)))
+  | "D", "NA" -> AES.CCM.decrypt ~key:key ~nonce:nonce (Cstruct.of_string(padding(readfile infile)))
   | "E", _  -> Some(AES.CCM.encrypt ~key:key ~nonce:nonce ~adata:(Cstruct.of_string(adata)) (Cstruct.of_string(padding(readfile infile))))
   | "D", _ -> AES.CCM.decrypt ~key:key ~nonce:nonce ~adata:(Cstruct.of_string(adata)) (Cstruct.of_string(padding(readfile infile)))
   | _ -> failwith "please enter E or D" in
   match coding with
-  | None -> failwith "failed"
+  | None -> failwith "nooutput"
   | Some(coding) -> savefile outfile (Cstruct.to_string coding)
 
   (*commandline interface start here*)
