57b942b417919efcb1bf2cc5c9bc5100e088af01
Qiuying Lai <giulia.lai@gmail.com>
2015-07-23 14:41:04 +0100

    Update rand.ml

--- src/rand.ml
+++ src/rand.ml
@@ -16,62 +16,8 @@ rand [-out OUTFILE | --outfile=OUTFILE] [-seed RANDSEED | --randseed=RANDSEED] [
 *)
 open Hex
 open Cmdliner
+open Common
 (*implementation*)
-
-(*
-type encoding = Base64 | Hex | NoEncoding
-
-let encode_str = function Base64 -> "base64" | Hex -> "hex" | NoEncoding -> "noencoding"
-
-let rand outfile randseed encode = 
-  let chan = open_in randseed in
-  try while true do let inputseed = input_line chan in
-  let generatedRand =
-    match inputseed with
-    |"noseed" -> Nocrypto.Rng.generate (*TO DO: use nocrypto lib generate pseudo rand*)
-    | _ -> Nocrypto.Rng.create ~seed:inputseed in (*TO DO: sort out seeded generation*)
-  let encodedRand = 
-    match encode with
-    | "base64" -> Nocrypto.Base64.encode generatedRand(*use nocrypto to encode randbytes with base 64 encoding*)
-    | "hex" -> Hex.of_string generatedRand (*hex encoding of randbytes*)
-    | "noencoding" -> generatedRand in
-  let save file string =
-    let channel = open_out file in
-    output_string channel string;
-    close_out channel in
-  match outfile with
-  | Some x -> save x encodedRand
-  | "NoOutput" -> print_endline encodedRand
-(* commandline interface*)
-
-  let outfile =
-  let doc = "Write to file instead of standard output." in
-  Arg.(required & pos_left ~rev:true 0 (some string) file [] & info ["out"; "outfile"] ~docv:"OUTFILE" ~doc)
-
-let randseed = 
-  let doc = "user provide seed" in
-  Arg.(required & pos"" & info ["seed"; "randseed"] ~docv:"RANDSEED" ~doc)
-
-let encode = 
-  let doc = "perform base64/PEM encoding" in
-  let base64 = Base64, Arg.info ["base64"; "pem"] ~doc in
-  let doc = "perform hex encoding" in 
-  let hex = Hex, Arg.info ["hex"] ~doc in
-  let doc = "neither base64 or hex encoding required" in
-  let noencoding = NoEncoding, Arg.info ["none";"noencoding"] ~doc in 
-  Arg.(last & vflag_all [NoEncoding] [base64; hex; noencoding])
-
-let cmd =
-  let doc = "rand" in
-  let man = [
-  `S "BUGS";
-  `P "Email to <giulia.lai@gmail.com>." ] in
-Term.(ret (pure rand $ outfile $ randseed $ encode)),
-Term.info "rand" ~version:rand_version.currentversion ~doc ~man
-
-let () = match Term.eval cmd with
-`Error _ -> exit 1 | _ -> exit 0
-*)
 
 let ()= Nocrypto_entropy_unix.initialize ()
 
@@ -82,15 +28,6 @@ let main (length:int) (encoding:encode)=
   match encoding with
   |Noencode |Hex -> bytes
   |Base64 -> Nocrypto.Base64.encode bytes
-
-let savefile afile thingtobesaved =
-  let channel = open_out afile in
-  output_string channel thingtobesaved;
-  close_out channel 
-
-let readfile bfile =
-  let channel = open_in bfile in
-  Std.input_all channel
 
 let number = 64
 let encode = Base64
