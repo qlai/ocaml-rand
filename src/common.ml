119ac938c31c358d2345c54c7b8a6221a7defcf1
Qiuying Lai <giulia.lai@gmail.com>
2015-08-03 16:24:36 +0100

    Update common.ml

--- src/common.ml
+++ src/common.ml
@@ -8,6 +8,19 @@ let savefile afile thingtobesaved =
 let readfile bfile =
   let channel = open_in bfile in
   Std.input_all channel
+  
+let explode s =
+  let rec expl i l =
+    if i < 0 then l else
+    expl (i - 1) (s.[i] :: l) in
+  expl (String.length s - 1) [];;
+
+let implode l =
+  let result = String.create (List.length l) in
+  let rec imp i = function
+  | [] -> result
+  | c :: l -> result.[i] <- c; imp (i + 1) l in
+  imp 0 l;;
 
 let infile =
   let doc = "filein" in
