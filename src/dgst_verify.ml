0c9dd1368a6b7e41162afe4f4d17a4dd9eb6c8bc
Qiuying Lai <giulia.lai@gmail.com>
2015-08-04 18:28:34 +0100

    Update dgst_verify.ml

--- src/dgst_verify.ml
+++ src/dgst_verify.ml
@@ -11,6 +11,19 @@ let sigencode keyfile msg =
   let key = Encoding.Pem.Private_key.of_pem_cstruct1 (load keyfile) in
   Rsa.PKCS1.sig_encode ~key:key msg
   
-let sigdecode keyfile msg =
+let sigdecode keyfile signature =
   let key = Encoding.Pem.Public_key.of_pem_cstruct1 (load keyfile) in
-  Rsa.PKCS1.sig_decode ~key:key msg
+  Rsa.PKCS1.sig_decode ~key:key signature
+
+let encryptmsg keyfile msg =
+  let key = Encoding.Pem.Public_key.of_pem_cstruct1 (load keyfile) in
+  Rsa.PKCS1.encrypt ~key:key msg
+  
+let decryptingciph keyfile msg =
+  let key = Encoding.Pem.Private_key.of_pem_cstruct (load keyfile) in 
+  let decrmsg = Rsa.PKCS.decrypt ~key:key msg in
+  match decrmsg with
+    | None -> failwish "no match"
+    | Some x -> x
+  
+  
