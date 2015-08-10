5d796a288ea50b02ce90464626715d3778942656
qlai <giulia.lai@gmail.com>
2015-08-10 15:53:33 +0100

    dgst compiles with verify options, but not final

--- src/common.ml
+++ src/common.ml
@@ -16,10 +16,10 @@ let explode s =
   expl (String.length s - 1) [];;
 
 let implode l =
-  let result = Bytes .create (List.length l) in
+  let result = Bytes.create (List.length l) in
   let rec imp i = function
   | [] -> result
-  | c :: l -> result.[i] <- c; imp (i + 1) l in
+  | c :: l -> Bytes.set result i c; imp (i + 1) l in
   imp 0 l;;
 
 let infile =
