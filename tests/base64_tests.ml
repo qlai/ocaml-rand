ab4a4da883a45bc528f53f10eec89659923cc6d5
Qiuying Lai <giulia.lai@gmail.com>
2015-08-12 16:23:30 +0100

    Update base64_tests.ml

--- tests/base64_tests.ml
+++ tests/base64_tests.ml
@@ -10,12 +10,16 @@ let readfile bfile =
 let a_ossl = readfile "docs/afile.txt"
 let b_ossl = readfile "docs/bfile.txt"
 let c_ossl = readfile "docs/cfile.txt"
-let d_ossl = readfile "docs/dfile.txt"
 
 let test1 test_ctxt = assert_equal a_ossl (Rand.b64enc E "docs/afiled.txt")
 let test2 test_ctxt = assert_equal b_ossl (Rand.b64enc E "docs/bfiled.txt")
+let test3 test_ctxt = assert_equal c_ossl (Rand.b64enc E "docs/cfiled.txt")
 
-(*
-"base64_testsuite" >::: [
-  "test1" >:: (fun _ -> ());
-  *)
+let base64suite =
+    "base64_testsuite" >::: 
+    [ "test1" >:: test1;
+      "test2" >:: test2;
+      "test3" >:: test3 ]
+      
+let () =
+    run_test_tt_main suite
