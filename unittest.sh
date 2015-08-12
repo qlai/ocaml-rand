efd914db55c4618be8e5919d2b7a7b5e726eba32
Qiuying Lai <giulia.lai@gmail.com>
2015-08-12 21:23:24 +0100

    Update unittest.sh

--- unittest.sh
+++ unittest.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 # $1 = infile
+# $2 = infilewithbase64
 
 openssl enc -base64 -in $1 -out out.tmp1
-./base64enc.byte $1 out.tmp2
+./base64enc.byte -e --in $1 --out out.tmp2
 diff out.tmp1 out.tmp2;
 test=$?;
-if [ "$test" -eq 0 ];
+openssl enc -base64 -d -in $2 -out out.tmp3
+./base64enc.byte -d --in $2 --out out.tmp4
+test2=$?;
+if [ "$test" -eq 0 -a "$test2" -eq 0];
 then # Do the thing if you get correct answer
   echo "Victory"
 else # Do the thing if you get wrong answer
@@ -15,5 +19,7 @@ fi;
 
 rm out.tmp1;
 rm out.tmp2;
+rm out.tmp3;
+rm out.tmp4;
 
 exit 0
