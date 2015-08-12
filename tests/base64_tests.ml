13befc76ae2b6348bbd42c7f362b1e6154a7ca31
qlai <giulia.lai@gmail.com>
2015-08-12 17:30:21 +0100

    minor

--- tests/base64_tests.ml
+++ tests/base64_tests.ml
@@ -11,7 +11,7 @@ let a_ossl = readfile "docs/afile.txt"
 let b_ossl = readfile "docs/bfile.txt"
 let c_ossl = readfile "docs/cfile.txt"
 
-let test1 test_ctxt = assert_equal a_ossl (Rand.b64enc E "docs/afiled.txt")
-let test2 test_ctxt = assert_equal b_ossl (Rand.b64enc E "docs/bfiled.txt")
-let test3 test_ctxt = assert_equal c_ossl (Rand.b64enc E "docs/cfiled.txt")
+let test1 test_ctxt = assert_equal a_ossl (Rand.Base64.b64enc E "docs/afiled.txt")
+let test2 test_ctxt = assert_equal b_ossl (Rand.Base64.b64enc E "docs/bfiled.txt")
+let test3 test_ctxt = assert_equal c_ossl (Rand.Base64.b64enc E "docs/cfiled.txt")
 
