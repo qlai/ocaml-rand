d2224ba2366f73fdb1ce18bcb1d88a5cd2c76e26
qlai <giulia.lai@gmail.com>
2015-08-12 15:14:17 +0100

    test dir and docs added

--- src/encode/base64enc.ml
+++ src/encode/base64enc.ml
@@ -1,26 +1,21 @@
 open Cmdliner
 open Common
 
-let base64enc code infile outfile =
-  let coding = match code with
-  | "E" -> Nocrypto.Base64.encode
-  | "D" -> Nocrypto.Base64.decode
-  | _ -> failwith "Please use either 'E' or 'D' for encoding or decoding" in
+let base64enc encode infile outfile =
+  let coding = match encode with
+  | E -> Nocrypto.Base64.encode
+  | D -> Nocrypto.Base64.decode in
   savefile outfile (Cstruct.to_string (coding (Cstruct.of_string (readfile infile))))
 
  (*commandline interface*)
 
-let code = 
-  let doc = "Enter E for encoding and D for decoding" in
-  Arg.(value & pos 0 string "E" & info [] ~doc)
-
 let cmd =
-  let doc = "encodes/decodes to/from base64; enter 'E' or 'D' before other args for encoding or decoding (default=E)" in
+  let doc = "performs base 64 encoding and decoding" in
   let man = [
     `S "BUGS";
     `P "Submit at https://github.com/qlai/ocaml-rand"]
   in
-  Term.(pure base64enc $ code $ infile $ outfile ),
+  Term.(pure base64enc $ encode $ infile $ outfile ),
   Term.info "base64enc" ~version:"0.0.1" ~doc ~man
 
 let () = match Term.eval cmd with `Error _ -> exit 1 | _ -> exit 0
