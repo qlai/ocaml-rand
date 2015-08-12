8e2532a07983a3b4e376adaafaccc30bcd322d40
Qiuying Lai <giulia.lai@gmail.com>
2015-08-12 16:28:58 +0100

    Update base64_tests.ml

--- tests/base64_tests.ml
+++ tests/base64_tests.ml
@@ -15,11 +15,3 @@ let test1 test_ctxt = assert_equal a_ossl (Rand.b64enc E "docs/afiled.txt")
 let test2 test_ctxt = assert_equal b_ossl (Rand.b64enc E "docs/bfiled.txt")
 let test3 test_ctxt = assert_equal c_ossl (Rand.b64enc E "docs/cfiled.txt")
 
-let base64suite =
-    "base64_testsuite" >::: 
-    [ "test1" >:: test1;
-      "test2" >:: test2;
-      "test3" >:: test3 ]
-      
-let () =
-    run_test_tt_main suite
