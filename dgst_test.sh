ec2f98401d4e537d8c78cc64f961b951eaf80b25
Qiuying Lai <giulia.lai@gmail.com>
2015-08-17 16:35:31 +0100

    hmac tests

--- dgst_test.sh
+++ dgst_test.sh
@@ -2,8 +2,23 @@
 
 # $1 input file
 
-openssl -dgst 
-./dgst.byte --
+# hmac tests
 
+hashfuncs='md5 sha1 sha224 sha256 sha385 sha512'
+
+for hashfun in $hashfuncs
+openssl -dgst -$hashfun -hmac "helloworld" -out out.tmp1 $1
+./dgst.byte --$hashfun --hmac "helloworld" --out out.tmp2 --in $1
+diff out.tmp1 out.tmp2;
+test=$?;
+if [ "$test" -eq 0 ];
+then 
+  echo $hashfun "hmac test passed"
+else
+  echo $hashfun "hmac test failed"
+fi
+
+rm out.tmp1;
+rm out.tmp2;
 
 exit 0
