90e861f97583632b857f676b1211cdcbab37e1ff
qlai <giulia.lai@gmail.com>
2015-08-17 16:11:07 +0100

    tests

---
+++ base64_test.sh
@@ -0,0 +1,36 @@
+#!/bin/bash 
+
+# $1 = infile
+
+openssl enc -base64 -in $1 -out out.tmp1
+./base64enc.byte -e --in $1 --out out.tmp2
+diff out.tmp1 out.tmp2;
+test=$?;
+openssl enc -base64 -d -in out.tmp1 -out out.tmp3
+./base64enc.byte -d --in out.tmp2 --out out.tmp4
+diff out.tmp3 out.tmp4;
+test2=$?;
+if [ "$test" -eq 0 -a "$test2" -eq 0 ];
+then # Do the thing if you get correct answer
+  echo "test passed"
+else # Do the thing if you get wrong answer
+  if [ "$test" -eq 1 ]
+    then 
+      if [ "$test2" -eq 1 ]
+      then 
+        echo "both enco/deco failed"
+      else 
+        echo "decode passed, encode failed"
+      fi
+    else
+      echo "encode passed, decode failed"
+    fi
+fi;
+
+rm out.tmp1;
+rm out.tmp2;
+rm out.tmp3;
+rm out.tmp4;
+
+
+exit 0
---
+++ dgst_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# $1 input file
+
+openssl -dgst 
+./dgst.byte --
+
+
+exit 0
