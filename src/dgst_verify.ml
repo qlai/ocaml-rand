ed925761fb28a117d14c910216bd8098ceb53c52
QLai <giulia.lai@gmail.com>
2015-08-24 17:16:38 +0100

    Update dgst_verify.ml

--- src/dgst_verify.ml
+++ src/dgst_verify.ml
@@ -7,10 +7,11 @@ let cs_mmap file =
 let load file =
   cs_mmap (file^".pem")
 
-let sigencode prkeyfile msg = 
+let sigencode prkeyfile msg outfile = 
   let key = match Encoding.Pem.Private_key.of_pem_cstruct1 (load prkeyfile) with
   |`RSA x -> x in
-  print_endline Rsa.PKCS1.sig_encode ~key:key msg
+  let signedmsg = Rsa.PKCS1.sig_encode ~key:key msg in
+  if outfile <> "NA" then savefile outfile signedmsg else print_endline signedmsg
   
 let sigdecode keyfile signature =
   let key = match Encoding.Pem.Public_key.of_pem_cstruct1 (load keyfile) with
@@ -42,11 +43,11 @@ let prsigdecode prkeyfile signature =
   | Some x -> print_endline "Verified OK!"
   | None -> failwith "signature invalid"
 
-let signandverify sign verify prverify signature msg =
+let signandverify sign verify prverify signature msg outfile =
   match sign, verify, prverify with 
     | "NA", "NA", "NA" -> ()
-    | "NA", "NA", _ -> if signature <> "NA" then prsigdecode prverify signature else failwith "no signature file"
-    | "NA", _, "NA" -> if signature <> "NA" then sigdecode verify signature else failwith "no signature file"
-    | _, "NA", "NA" -> sigencode sign msg
+    | "NA", "NA", _ -> if signature <> "NA" then prsigdecode prverify (readfile signature) else failwith "no signature file"
+    | "NA", _, "NA" -> if signature <> "NA" then sigdecode verify (readfile signature) else failwith "no signature file"
+    | _, "NA", "NA" -> sigencode sign msg outfile
     | "NA", _, _ | _, "NA", _ | _, _, "NA" | _, _, _-> failwith "error in calling too many functions"
     
